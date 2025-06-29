41089973dc722296949bb409fddfbf6e8888eb7d
VED <vsjadhav1085@gmail.com>
2025-06-30 01:39:32 +0530

    Connected app to Railway MySQL

--- db_config.py
+++ db_config.py
@@ -1,8 +1,10 @@
 from flask_mysqldb import MySQL
 
 def init_db(app):
-    app.config['MYSQL_HOST'] = 'localhost'
+    # MySQL Configuration for Railway DB
+    app.config['MYSQL_HOST'] = 'turntable.proxy.rlwy.net'
+    app.config['MYSQL_PORT'] = 45859
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_PASSWORD'] = 'GeVuCRNxGxYqoCDuRSnZhlFSYJPhPtio'
     app.config['MYSQL_DB'] = 'battery_swap'
     return MySQL(app)
